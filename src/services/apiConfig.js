9f73005f0786f64b1d53e27a47af549fbba9effc
riyansh-dkg <riyansh@dkgrouplabs.com>
2026-03-06 15:01:52 +0530

    uat- prod

--- src/services/apiConfig.js
+++ src/services/apiConfig.js
@@ -5,7 +5,8 @@
 
 
 export const getBaseUrl = () => {
-  return "https://sarthibackendservice-bfe2eag3byfkbsa6.canadacentral-01.azurewebsites.net/sarthi-backend/api";
+  // return "https://sarthibackendservice-bfe2eag3byfkbsa6.canadacentral-01.azurewebsites.net/sarthi-backend/api";
+  return "https://api.ritesqasarthi.com/sarthi-backend/api";
 };
 
 // API Endpoints configuration
