f5d05dda968c612f1e12e4cb06a5fd2072a56ba4
Kishore DKG <kishore@dkgrouplabs.com>
2026-05-16 11:20:19 +0530

    Update API_BASE_URL for production deployment
    Comment out local API base URL logic and set production API base URL.

--- railpad/src/services/config.js
+++ railpad/src/services/config.js
@@ -1,11 +1,11 @@
-const isLocal = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
+// const isLocal = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
 
-export const API_BASE_URL = isLocal
-   ? 'http://localhost:8080/sarthi-backend/api'
-   : 'https://sarthibackendservice-bfe2eag3byfkbsa6.canadacentral-01.azurewebsites.net/sarthi-backend/api';
+// export const API_BASE_URL = isLocal
+//    ? 'http://localhost:8080/sarthi-backend/api'
+//    : 'https://sarthibackendservice-bfe2eag3byfkbsa6.canadacentral-01.azurewebsites.net/sarthi-backend/api';
 
 // For Deployment:
-// export const API_BASE_URL = "https://api.ritesqasarthi.com/sarthi-backend/api";
+ export const API_BASE_URL = "https://api.ritesqasarthi.com/sarthi-backend/api";
 // export const API_BASE_URL =  "https://sarthibackendservice-bfe2eag3byfkbsa6.canadacentral-01.azurewebsites.net/sarthi-backend/api";
 
 export const API_CONFIG = {
